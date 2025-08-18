0f6d6aa29a07fe99d25781e05d981d7ab412ed61
Martijn van den Bosch <martijn_van_den_bosch@hotmail.com>
2025-08-18 08:46:08 +0200

    Improved Integer

--- src/tests/integer.test.ts
+++ src/tests/integer.test.ts
@@ -15,6 +15,30 @@ describe('Integer class', () => {
         }
     });
 
+    test('It sets the correct value from Number', () => {
+        const values = [
+            Number(Number.MIN_SAFE_INTEGER),
+            Number(-99),
+            Number(-1),
+            Number(0),
+            Number(1),
+            Number(99),
+            Number(Number.MAX_SAFE_INTEGER),
+        ];
+
+        for (const value of values) {
+            expect(new Integer(value).value).toBe(value);
+        }
+    });
+
+    test('It throws an error when the value is out of allowed range', () => {
+        const values = [Number.MIN_SAFE_INTEGER - 1, Number.MAX_SAFE_INTEGER + 1];
+
+        for (const value of values) {
+            expect(() => new Integer(value)).toThrowError(TypeVaultValidationError);
+        }
+    });
+
     test('It throws an error if the value is not a number', () => {
         const values = ['foo', {}, null, true, false, [], [1, 2, 3], { foo: 'bar' }, BigInt(1)];
 
@@ -36,7 +60,34 @@ describe('Integer class', () => {
     });
 
     test('It floors the value', () => {
-        const values = [2, -1.9, -1.1, -1, -0.9, -0.1, 0, 0.1, 0.9, 1, 1.1, 1.9, 2];
+        const values = [
+            -2,
+            -1.9,
+            -1.1,
+            -1,
+            -0.9,
+            -0.1,
+            0,
+            0.1,
+            0.9,
+            1,
+            1.1,
+            1.9,
+            2,
+            Number(-2),
+            Number(-1.9),
+            Number(-1.1),
+            Number(-1),
+            Number(-0.9),
+            Number(-0.1),
+            Number(0),
+            Number(0.1),
+            Number(0.9),
+            Number(1),
+            Number(1.1),
+            Number(1.9),
+            Number(2),
+        ];
 
         for (const value of values) {
             expect(new Integer(value).value).toBe(Math.floor(value));
--- src/types/integer.ts
+++ src/types/integer.ts
@@ -6,7 +6,27 @@ export class Integer extends Type<number> {
     }
 
     protected validate(value: unknown): boolean {
-        return typeof value === 'number' && !Number.isNaN(value) && Number.isFinite(value);
+        if (!(typeof value === 'number' || value instanceof Number)) {
+            return false;
+        }
+
+        if (Number.isNaN(value)) {
+            return false;
+        }
+
+        if (!Number.isFinite(value)) {
+            return false;
+        }
+
+        if ((value as number) < Number.MIN_SAFE_INTEGER) {
+            return false;
+        }
+
+        if ((value as number) > Number.MAX_SAFE_INTEGER) {
+            return false;
+        }
+
+        return true;
     }
 
     protected modifier(value: number): number {
