b91e4749abf37feb1f0b745a6b24fb5969fd79e2
Martijn van den Bosch <martijn_van_den_bosch@hotmail.com>
2025-08-26 22:52:43 +0200

    Cleanup

--- src/utils/nullable.ts
+++ src/utils/nullable.ts
@@ -10,20 +10,6 @@ export function Nullable<TValue, TInstance extends Type<TValue>>(instance: TInst
     return new (class Nullable extends (instance.constructor as Constructor) {
         protected _value: TValue | null | undefined = instance.rawValue;
 
-        // public get value(): TValue | null {
-        //     if (this._value === undefined) {
-        //         return this.default();
-        //     }
-
-        //     return this._value;
-        // }
-
-        // public set value(value: TValue | null) {
-        //     this._value = value ? value : undefined;
-
-        //     instance.value = value;
-        // }
-
         protected default() {
             return null;
         }
