60a085cc31ff033cc91eec9bd44981f3e433c619
Martijn van den Bosch <martijn_van_den_bosch@hotmail.com>
2025-08-29 08:20:17 +0200

    Added more immutable tests

--- src/tests/immutable.test.ts
+++ src/tests/immutable.test.ts
@@ -1,5 +1,9 @@
+import { Email } from '@/types/email.js';
 import { Integer } from '@/types/integer.js';
+import { Uuid } from '@/types/uuid.js';
+import { Uuidv4 } from '@/types/uuidv4.js';
 import { Immutable } from '@/utils/immutable.js';
+import { Nullable } from '@/utils/nullable.js';
 import { describe, expect, test } from 'vitest';
 
 describe('Immutable function', () => {
@@ -9,12 +13,53 @@ describe('Immutable function', () => {
         expect(immutable instanceof Integer).toBeTruthy();
     });
 
-    test('It expects instance of Integer', () => {
+    test('It throws an error when trying to set the value', () => {
         const immutable = Immutable(new Integer());
 
         expect(() => {
-            // @ts-expect-error This is not allowed
+            // @ts-expect-error Type error expected
             immutable.value = 1;
+        }).toThrowError(Error);
+    });
+
+    test('It throws an error when trying to set the value of nullable', () => {
+        const immutable = Immutable(Nullable(new Integer()));
+
+        expect(() => {
+            // @ts-expect-error Type error expected
+            immutable.value = 1;
+        }).toThrowError(Error);
+        expect(immutable.value).toBe(null);
+    });
+
+    test('It expects instance of Uuid', () => {
+        const immutable = Immutable(Uuidv4.random());
+
+        expect(immutable instanceof Uuid).toBeTruthy();
+        expect(immutable instanceof Uuidv4).toBeTruthy();
+    });
+
+    test('It throws an error when trying to set the value of Uuid', () => {
+        const immutable = Immutable(Uuidv4.random());
+
+        expect(() => {
+            // @ts-expect-error Type error expected
+            immutable.value = 'foo';
+        }).toThrowError(Error);
+    });
+
+    test('It expects instance of Email', () => {
+        const immutable = Immutable(new Email('foo@example.com'));
+
+        expect(immutable instanceof Email).toBeTruthy();
+    });
+
+    test('It throws an error when trying to set the value of Email', () => {
+        const immutable = Immutable(new Email('foo@example.com'));
+
+        expect(() => {
+            // @ts-expect-error Type error expected
+            immutable.value = 'foo';
         }).toThrowError(Error);
     });
 });
