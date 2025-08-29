2bf29de3c158a3f5c60a8f544d336c08935d0293
Martijn van den Bosch <martijn_van_den_bosch@hotmail.com>
2025-08-29 08:20:05 +0200

    Better email typing

--- src/types/email.ts
+++ src/types/email.ts
@@ -1,9 +1,10 @@
 import { TypeVaultValidationError } from '@/errors/typeVaultValidationError.js';
+import { Email as EmailPattern } from '@/utils/types.js';
 import * as z from 'zod/mini';
 import { BaseString } from './baseString.js';
 
 export class Email extends BaseString {
-    public constructor(value: string) {
+    public constructor(value: EmailPattern) {
         if (!validate(value)) {
             throw new TypeVaultValidationError();
         }
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,3 +1,75 @@
 export type Brand<TInstance, TBrand> = TInstance & { __brand: TBrand };
 
 export type Constructor = new (...args: unknown[]) => object;
+
+type Unpack<T> = {
+    [K in keyof T]: T[K] extends object ? Unpack<T[K]> : T[K];
+};
+
+export type LetterLowerCase =
+    | 'a'
+    | 'b'
+    | 'c'
+    | 'd'
+    | 'e'
+    | 'f'
+    | 'g'
+    | 'h'
+    | 'i'
+    | 'j'
+    | 'k'
+    | 'l'
+    | 'm'
+    | 'n'
+    | 'o'
+    | 'p'
+    | 'q'
+    | 'r'
+    | 's'
+    | 't'
+    | 'u'
+    | 'v'
+    | 'w'
+    | 'x'
+    | 'y'
+    | 'z';
+
+export type LetterUpperCase =
+    | 'A'
+    | 'B'
+    | 'C'
+    | 'D'
+    | 'E'
+    | 'F'
+    | 'G'
+    | 'H'
+    | 'I'
+    | 'J'
+    | 'K'
+    | 'L'
+    | 'M'
+    | 'N'
+    | 'O'
+    | 'P'
+    | 'Q'
+    | 'R'
+    | 'S'
+    | 'T'
+    | 'U'
+    | 'V'
+    | 'W'
+    | 'X'
+    | 'Y'
+    | 'Z';
+
+export type Letter = Unpack<LetterLowerCase | LetterUpperCase>;
+
+export type Digit = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9';
+
+export type Alphanumeric = Unpack<Letter | Digit>;
+
+export type Tld = Unpack<`${Letter}${string}`>;
+
+export type Domain = Unpack<`${string}.${Tld}`>;
+
+export type Email = Unpack<`${string}@${Domain}`>;
