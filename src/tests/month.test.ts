1ec71ac94546a85559013ae0cd6c308881f7cc3d
Martijn van den Bosch <martijn_van_den_bosch@hotmail.com>
2025-09-03 08:42:36 +0200

    Added month

---
+++ src/tests/month.test.ts
@@ -0,0 +1,43 @@
+import { TypeVaultValidationError } from '@/errors/typeVaultValidationError.js';
+import { Month } from '@/types/month.js';
+import { MonthString } from '@/utils/types.js';
+import { describe, expect, test } from 'vitest';
+
+describe('Month class', () => {
+    test('It sets the corrent value', () => {
+        [
+            'january',
+            'february',
+            'march',
+            'april',
+            'may',
+            'june',
+            'july',
+            'august',
+            'september',
+            'october',
+            'november',
+            'december',
+        ].forEach((value) => {
+            const month = new Month(value as MonthString);
+
+            expect(month.value).toBe(value);
+        });
+    });
+
+    test('It throws an error if the value is not a string', () => {
+        const values = [1, {}, true, false, [], [1, 2, 3], { foo: 'bar' }, BigInt(1)];
+
+        for (const value of values) {
+            expect(() => new Month(value as unknown as MonthString)).toThrowError(
+                TypeVaultValidationError
+            );
+        }
+    });
+
+    test('It return an error when the value is not a valid month', () => {
+        expect(() => new Month('foo' as unknown as MonthString)).toThrowError(
+            TypeVaultValidationError
+        );
+    });
+});
---
+++ src/types/month.ts
@@ -0,0 +1,48 @@
+import { MonthString } from '@/utils/types.js';
+import { BaseString } from './baseString.js';
+
+export class Month extends BaseString<MonthString | Capitalize<MonthString>> {
+    public static January = new Month('january');
+    public static February = new Month('february');
+    public static March = new Month('march');
+    public static April = new Month('april');
+    public static May = new Month('may');
+    public static June = new Month('june');
+    public static July = new Month('july');
+    public static August = new Month('august');
+    public static September = new Month('september');
+    public static October = new Month('october');
+    public static November = new Month('november');
+    public static December = new Month('december');
+
+    protected modifier(value: unknown) {
+        if (value instanceof Month) {
+            value = value.value;
+        }
+
+        return value as MonthString;
+    }
+
+    protected validate(value: string): boolean {
+        if (!super.validate(value)) {
+            return false;
+        }
+
+        return [
+            'january',
+            'february',
+            'march',
+            'april',
+            'may',
+            'june',
+            'july',
+            'august',
+            'september',
+            'october',
+            'november',
+            'december',
+        ].some((month) => {
+            return month === value.toLowerCase();
+        });
+    }
+}
