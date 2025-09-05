b7d552eed8f3c0fe61d52881292813787560bdf7
Martijn van den Bosch <martijn_van_den_bosch@hotmail.com>
2025-09-05 08:59:49 +0200

    Added nullable and immutable static methods

--- src/types/integer.ts
+++ src/types/integer.ts
@@ -10,4 +10,12 @@ export class Integer<TOptions extends TypeOption = TypeOption> extends BaseNumbe
 
         return Math.floor(Number(value));
     }
+
+    public static nullable(value: number | null = null) {
+        return new Integer(value, { nullable: true });
+    }
+
+    public static immutable(value: number) {
+        return new Integer(value, { immutable: true });
+    }
 }
