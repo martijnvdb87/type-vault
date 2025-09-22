a2c63d83dddc8cbb28525de01d7155552266d2f5
Martijn van den Bosch <martijn_van_den_bosch@hotmail.com>
2025-09-22 08:46:32 +0200

    Removed unused

--- src/types/color.ts
+++ src/types/color.ts
@@ -4,10 +4,4 @@ import { TypeOption } from './type.js';
 export abstract class Color<
     TOptions extends TypeOption = TypeOption,
     TValue extends string = string,
-> extends BaseString<TOptions, TValue> {
-    public abstract get alpha(): number;
-
-    public getNormalizedAlpha() {
-        return this.alpha;
-    }
-}
+> extends BaseString<TOptions, TValue> {}
