20008944085621933ec4d5b60001bc921255967b
Martijn van den Bosch <martijn_van_den_bosch@hotmail.com>
2025-10-02 08:58:17 +0200

    Updated index exports

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,22 @@
 export { Type, TypeOption, TypeValue } from './types/type.js';
 
+export { Color } from './types/color.js';
+export { ColorHex } from './types/colorHex.js';
+export { ColorHsl } from './types/colorHsl.js';
+export { ColorOklch } from './types/colorOklch.js';
+export { ColorRgb } from './types/colorRgb.js';
 export { DateOnly } from './types/dateOnly.js';
 export { DateTime } from './types/dateTime.js';
+export { Duration } from './types/duration.js';
 export { Email } from './types/email.js';
 export { Float } from './types/float.js';
 export { Integer } from './types/integer.js';
+export { Month } from './types/month.js';
 export { Text } from './types/text.js';
 export { TimeOnly } from './types/timeOnly.js';
 export { Url } from './types/url.js';
 export { Uuid } from './types/uuid.js';
+export { Weekday } from './types/weekday.js';
 
 export { TypeVaultError } from './errors/typeVaultError.js';
 export { TypeVaultValidationError } from './errors/typeVaultValidationError.js';
